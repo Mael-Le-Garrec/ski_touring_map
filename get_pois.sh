fe4352513233fbd146dfac522a4d4496d0db2602
Maël Le Garrec <mael@legarrec.org>
2021-02-19 18:06:40 +0100

    Move the API call to bash instead of JS

---
+++ get_pois.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+save_pois()
+{
+  bounds="$1"
+  type_poi="$2"
+  save_file="$3"
+  
+  url="https://overpass-api.de/api/interpreter?data=[out:json][timeout:15];(node[$type_poi]($bounds);way[$type_poi]($bounds);relation[$type_poi]($bounds););out%20body%20geom;"
+  wget "$url" -O "$save_file"
+}
+
+
+# Those bounds are roughly the French Alps and a bit of Switzerland and Italy
+bounds="44.59946692494541,4.630125461347195,46.72319697007933,8.097280867457206"
+
+jsondir="./json"
+save_pois "$bounds" "tourism=alpine_hut" "$jsondir/alpine_hut.json"
+sleep 5
+save_pois "$bounds" "tourism=wilderness_hut" "$jsondir/wilderness_hut.json"
